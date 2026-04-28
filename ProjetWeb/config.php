8f86519f1f1055f28673a9b3809ec43fc11667a0
Yacine <MohamedYacine.BenMaouia.esprit.tn>
2026-04-28 22:28:28 +0100

    projet avec la premiere entitée

---
+++ ProjetWeb/config.php
@@ -0,0 +1,26 @@
+<?php
+
+class config
+{
+    private static $pdo = null;
+
+    public static function getConnexion()
+    {
+        if (!isset(self::$pdo)) {
+            try {
+                self::$pdo = new PDO(
+                    'mysql:host=localhost;dbname=projetweb',  // Base correcte
+                    'root',
+                    '',
+                    [
+                        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+                    ]
+                );
+            } catch (Exception $e) {
+                die('Erreur de connexion à la base de données : ' . $e->getMessage());
+            }
+        }
+        return self::$pdo;
+    }
+}
